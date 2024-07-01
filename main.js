8296eca457d457992b7fd7b917358aa866aab476
celtaptreto88 <129746944+celtaptreto88@users.noreply.github.com>
2024-07-01 08:55:29 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+const caixaPrincipal = document.querySelector(".Caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".Caixa-resultados");
+const caixaResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "pergunta 1",
+        alternativas: ["alternativa 1",
+            "alternativa 2"]
+    },
+
+    {
+        enunciado: "pergunta 2",
+    alternativas: ["alternativa 1",
+        "alternativa 2"]
+
+    }
+
+]
